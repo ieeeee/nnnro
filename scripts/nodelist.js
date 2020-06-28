6bf39feebb2dc7c28f660d9bef479a014fca9ebf
rikawav <rikawav@gmail.com>
2020-06-28 22:15:13 +0800

    init

--- scripts/nodelist.js
+++ scripts/nodelist.js
@@ -1,1 +1,11 @@
-
+(function () {
+    axios.get('https://nnn3ro.link/link/nkWATEM7Y5JBxvKP?mu=4', {
+    }).then(res => {
+        console.log('数据是:', res);
+        
+        nativeObject = YAML.parse(res);
+        console.log('数据是:', nativeObject);
+    }).catch((e) => {
+        console.log('获取数据失败');
+    });
+})();
